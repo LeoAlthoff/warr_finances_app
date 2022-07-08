88c11011f479a414ad94fabb96f9752d489360dd
William De Souza Fernandes <William.Fernandes@proway.treina>
2022-07-08 11:25:48 -0300

    Drawer Update

--- lib/presenter/home_page/home_page.dart
+++ lib/presenter/home_page/home_page.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 import 'widgets/body_home.dart';
@@ -38,41 +37,3 @@ class HomePage extends StatelessWidget {
 }
 
 
-=======
-import 'package:flutter/material.dart';
-
-import 'widgets/body_home.dart';
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 210, 52, 105),
-        title: const Text('Olá, Rodrigo'),
-        centerTitle: true,
-      ),
-      drawer: const Drawer(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.drag_handle_sharp),
-            label: "Extrato",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.attach_money),
-            label: "Cadastrar",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.stacked_bar_chart_outlined),
-            label: "Resumo",
-          ),
-        ],
-      ),
-      body: const BodyHome(),
-    );
-  }
-}
->>>>>>> fc17c945494476be42469e4317dda18955abd26b
--- lib/presenter/home_page/widgets/drawer_home.dart
+++ lib/presenter/home_page/widgets/drawer_home.dart
@@ -9,62 +9,89 @@ class DrawerHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 210, 52, 105),
-            ),
-            child: Column(
-              children: [
-                Text(
-                  'Rodrigo',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
+          SizedBox(
+            height: 130,
+            child: DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 210, 52, 105),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.person,
+                        color: Colors.white,
+                        size: 26,
+                      ),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Rodrigo',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+                  const SizedBox(height: 3),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '        rodrigo@gmail.com',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                Text(
-                  'rodrigo@gmail.com',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      'O que deseja fazer ?',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                  ),
-                Text(
-                  'O que deseja fazer ?',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
+                  const SizedBox(width: 15),
+                ],
+              ),
             ),
           ),
-          ListTile(
+          const ListTile(
             title: Text('Pesquisa'),
             leading: Icon(Icons.search),
           ),
-          ListTile(
+          const SizedBox(height: 7),
+          const ListTile(
             title: Text('Notificações'),
             leading: Icon(Icons.notifications),
           ),
-          ListTile(
+          const SizedBox(height: 7),
+          const ListTile(
             title: Text('Contato'),
             leading: Icon(Icons.contact_support_rounded),
           ),
-          ListTile(
+          const SizedBox(height: 7),
+          const ListTile(
             title: Text('Segurança'),
             leading: Icon(Icons.shield_outlined),
           ),
-          ListTile(
+          const SizedBox(height: 7),
+          const ListTile(
             title: Text('Configurações'),
             leading: Icon(Icons.settings),
           ),
-          ListTile(
+          const SizedBox(height: 7),
+          const ListTile(
             title: Text('Sair do aplicativo'),
             leading: Icon(Icons.arrow_back),
           ),
