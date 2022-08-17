b96b1ea3c3ab1e6ec0b4ec640c467f8e4bdb968d
Rodrigo Kapulka Franco <Rodrigo.Franco@proway.treina>
2022-08-17 14:07:48 -0300

    [fix] all imports

--- lib/presenter/register/widgets/body_register.dart
+++ lib/presenter/register/widgets/body_register.dart
@@ -1,7 +1,7 @@
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_teste_app/presenter/home/home_page.dart';
+import '../../home/home_page.dart';
 
 import '../../../shared/utils/database_helper.dart';
 import '../../../shared/utils/dateFormater.dart';
