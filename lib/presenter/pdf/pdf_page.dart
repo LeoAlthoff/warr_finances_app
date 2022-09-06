8b06a8bfc83c1a5a3a2a32149a8dfbc50c328ffa
Rodrigo Franco <rodrigokafra@hotmail.com>
2022-09-06 14:34:17 -0300

    [remove] datetimepicker on pdf

--- lib/presenter/pdf/pdf_page.dart
+++ lib/presenter/pdf/pdf_page.dart
@@ -8,9 +8,7 @@ import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
 import '../../dio/dio_helper.dart';
-import '../../shared/utils/is_dark.dart';
 import '../../shared/utils/shared_preferences.dart';
-import '../home/home_page.dart';
 
 class ShowPdf extends StatelessWidget {
   const ShowPdf(this.title, {Key? key}) : super(key: key);
@@ -53,47 +51,8 @@ class ShowPdf extends StatelessWidget {
     final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: true);
     final font = await PdfGoogleFonts.nunitoExtraLight();
 
-    final selectedDate = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2025),
-      builder: (context, child) {
-        return Theme(
-          data: Theme.of(context).copyWith(
-            colorScheme: ColorScheme.light(
-              primary: isDark(context)
-                  ? const Color.fromARGB(214, 238, 46, 94)
-                  : const Color.fromRGBO(
-                      238, 46, 93, 1), // header background color
-              onPrimary: Colors.white, // header text color
-              onSurface: isDark(context)
-                  ? Colors.white
-                  : Colors.black, // body text color
-            ),
-            textButtonTheme: TextButtonThemeData(
-              style: TextButton.styleFrom(
-                primary: isDark(context)
-                    ? const Color.fromARGB(214, 238, 46, 94)
-                    : const Color.fromRGBO(238, 46, 93, 1), // button text color
-              ),
-            ),
-          ),
-          child: child!,
-        );
-      },
-    );
-    if (selectedDate == null) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => HomePage(
-            currentPage: 0,
-          ),
-        ),
-      );
-    }
     final operations = await DioHelper.getOperations(
-      selectedDate!,
+      DateTime.now(),
       SharedPreferencesHelper.prefs!.getInt("UserId")!,
     );
     final iconPng =
